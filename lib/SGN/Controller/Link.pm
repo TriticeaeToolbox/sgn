626428c2a177af0f01c82cc56af101ce73bc1702
David Waring <dev@davidwaring.net>
2020-01-20 12:03:38 -0500

    Link: Handle Markers

--- lib/SGN/Controller/Link.pm
+++ lib/SGN/Controller/Link.pm
@@ -3,9 +3,6 @@ package SGN::Controller::Link;
 
 use Moose;
 
-use CXGN::Phenome::Schema;
-use CXGN::Stock;
-
 BEGIN { extends "Catalyst::Controller"; }
 
 
@@ -19,20 +16,28 @@ sub link : Path('/link') Args(0) {
     
     my $type = $c->request->query_parameters->{type};
     my $q = $c->request->query_parameters->{q};
-    my $id = undef;
     my $url = undef;
 
-    my $schema = $c->dbic_schema('Bio::Chado::Schema', 'sgn_chado');
-
+    # DB handle for queries
+    my $dbh = $c->dbc->dbh();
 
     # TYPE: Accessions
     if ( lc($type) eq 'accession' ) {
-        my $stock = $schema->resultset("Stock::Stock")->find({
-            'UPPER(me.uniquename)' => uc($q)
-        });
-        if ( defined $stock ) {
-            $id = $stock->stock_id;
-            $url = "/stock/$id/view";
+        my $h = $dbh->prepare("SELECT stock_id FROM public.stock WHERE UPPER(uniquename) = ?;");
+        $h->execute(uc($q));
+        my ($stock_id) = $h->fetchrow_array() and $h->finish();
+        if ( defined $stock_id ) {
+            $url = "/stock/$stock_id/view";
+        }
+    }
+
+    # TYPE: Markers
+    elsif ( lc($type) eq 'marker' ) {
+        my $h = $dbh->prepare("SELECT marker_id FROM sgn.marker_alias WHERE UPPER(alias) = ?");
+        $h->execute(uc($q));
+        my ($marker_id) = $h->fetchrow_array() and $h->finish();
+        if ( defined $marker_id ) {
+            $url = "/marker/SGN-M$marker_id/details";
         }
     }
 
