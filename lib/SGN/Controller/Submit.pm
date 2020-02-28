4adccab7654ffa11e400cb67f0c28b1ec0120872
David Waring <dev@davidwaring.net>
2020-02-28 13:22:28 -0500

    /submit --> /submit/pheno

--- lib/SGN/Controller/Submit.pm
+++ lib/SGN/Controller/Submit.pm
@@ -5,11 +5,11 @@ use Moose;
 BEGIN { extends "Catalyst::Controller"; }
 
 
-sub phenotype_upload_workflow : Path('/submit') Args(0) {
+sub phenotype_upload_workflow : Path('/submit/pheno') Args(0) {
     my $self = shift;
     my $c = shift;
 
-    $c->stash->{template} = '/submit/index.mas';
+    $c->stash->{template} = '/submit/pheno.mas';
 }
 
 1;
