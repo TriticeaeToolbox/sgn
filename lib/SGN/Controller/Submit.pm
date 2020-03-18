b61dc7c309400a7b9a35957e76bc28672ff8bebb
David Waring <dev@davidwaring.net>
2020-03-18 09:52:46 -0400

    Pheno Upload: Download individual file

--- lib/SGN/Controller/Submit.pm
+++ lib/SGN/Controller/Submit.pm
@@ -52,4 +52,46 @@ sub view_submission_files : Path('/submit/view') Args(2) {
     $c->stash->{template} = '/submit/view.mas';
 }
 
+
+sub view_submission_file : Path('/submit/view') Args(3) {
+    my $self = shift;
+    my $c = shift;
+    my $user_id = shift;
+    my $sub_dir = shift;
+    my $file = shift;
+
+    my $submission_path = $c->config->{submission_path};
+
+    # redirect to login page if not logged in
+    if (! $c->user()) { 
+        $c->res->redirect('/user/login?goto_url=' . $c->req->uri->path_query);
+        return;
+    }
+
+    # Build directory path
+    my $dir = $submission_path . "/" . $user_id . "/" . $sub_dir;
+
+    # Check directory exists
+    if ( !-d $dir ) {
+        $c->stash->{template} = 'generic_message.mas';
+        $c->stash->{message} = "<p><strong>Submission Not Found</strong></p><p>The directory <code>$dir</code> could not be found on this server.</p>";
+        return;
+    }
+
+    # Build file path
+    my $file_path = $dir . "/" . $file;
+
+    # Check file exists
+    if ( !-e $file_path ) {
+        $c->stash->{template} = 'generic_message.mas';
+        $c->stash->{message} = "<p><strong>Submission File Not Found</strong></p><p>The file <code>$file_path</code> could not be found on this server.</p>";
+        return;
+    }
+
+    # Serve the file
+    open my $fh, '<', $file_path;
+    $c->res->header('Content-Disposition', qq[attachment; filename="$file"]);
+    $c->res->body($fh);
+}
+
 1;
