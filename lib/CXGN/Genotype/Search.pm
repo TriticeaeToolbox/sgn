2faa2b1935affd719108f0f934b9ff53cb8a7346
Clay Birkett <clb343@cornell.edu>
2021-01-28 11:35:56 -0500

    fix errors running solgwas

--- lib/CXGN/Genotype/Search.pm
+++ lib/CXGN/Genotype/Search.pm
@@ -1325,7 +1325,7 @@ sub get_cached_file_dosage_matrix {
 
         #VCF should be sorted by chromosome and position
         no warnings 'uninitialized';
-        @all_marker_objects = sort { $a->{chrom} <=> $b->{chrom} || $a->{pos} <=> $b->{pos} || $a->{name} cmp $b->{name} } @all_marker_objects;
+        @all_marker_objects = sort { $a->{chrom} cmp $b->{chrom} || $a->{pos} <=> $b->{pos} || $a->{name} cmp $b->{name} } @all_marker_objects;
         @all_marker_objects = $self->_check_filtered_markers(\@all_marker_objects);
 
         my $counter = 0;
