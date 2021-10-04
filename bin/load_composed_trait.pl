32c5cd1918a7d8dd13a02c7e89b3331c626ee3ad
David Waring <djw64@cornell.edu>
2021-10-04 11:01:53 -0400

    Use composed trait ontology to store custom breeder traits

---
+++ bin/load_composed_trait.pl
@@ -0,0 +1,75 @@
+#! /usr/bin/env perl
+
+=head1
+
+load_composed_trait.pl
+
+=head1 SYNOPSIS
+
+load_composed_trait.pl -H [dbhost] -D [dbname] -T [trait name]
+
+=head1 COMMAND-LINE OPTIONS
+
+ -H  host name
+ -D  database name
+ -T  trait name
+
+=head2 DESCRIPTION
+
+Add a free-text trait (not connected to any other ontology terms) into the composed trait ontology
+
+=head2 AUTHOR
+
+David Waring <djw64@cornell.edu>
+
+=cut
+
+use strict;
+use warnings;
+use Data::Dumper;
+use Getopt::Std;
+use Bio::Chado::Schema;
+use CXGN::DB::InsertDBH;
+use CXGN::DB::Connection;
+use CXGN::Onto;
+use Try::Tiny;
+
+our ( $opt_H, $opt_D, $opt_T );
+getopts('H:D:T:');
+
+sub print_help {
+    print STDERR
+"A script to load a free-text trait into the composed trait ontology\nUsage: load_composed_trait.pl -H [dbhost] -D [dbname] -T [trait name] \n";
+}
+
+if ( !$opt_D || !$opt_H || !$opt_T ) {
+    print_help();
+    die("Exiting: -H [dbhost] or -D [dbname] or -T [trait name] options missing\n");
+}
+
+my $dbh = CXGN::DB::InsertDBH->new(
+    {
+        dbname => $opt_D,
+        dbhost => $opt_H,
+        dbargs => {
+            AutoCommit => 1,
+            RaiseError => 1
+        },
+    }
+);
+
+my $schema = Bio::Chado::Schema->connect( sub { $dbh->get_actual_dbh() } );
+
+my $coderef = sub {
+    my $onto = CXGN::Onto->new({ schema => $schema });
+    $onto->store_breeder_term($opt_T);
+};
+
+try {
+    $schema->txn_do($coderef);
+}
+catch {
+    die "Load failed! " . $_ . "\n";
+};
+
+print "You're done!\n";
