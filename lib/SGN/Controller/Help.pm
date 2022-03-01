a7b58e8d5705b4ff125165634c28ece3c66a8549
David Waring <djw64@cornell.edu>
2022-03-01 17:20:59 -0500

    Generate Sample Templates: add page for downloading templates

--- lib/SGN/Controller/Help.pm
+++ lib/SGN/Controller/Help.pm
@@ -2,6 +2,7 @@
 package SGN::Controller::Help;
 
 use Moose;
+use CXGN::People::Person;
 
 BEGIN { extends "Catalyst::Controller"; }
 
@@ -33,6 +34,21 @@ sub help_section : Path('/help') Args(1) {
     	$c->stash->{template} = '/generic_message.mas';
 	$c->stash->{message}  = 'The requested page could not be found. <br /><a href="/help">Help page</a>';
     }
+}
+
+sub help_generate_sample_tempaltes : Path('/help/sample_templates') Args(0) {
+    my $self = shift;
+    my $c = shift;
+
+    # Set intials, if logge in
+    if ($c->user()) {
+        my $logged_in_person_id = $c->user()->get_sp_person_id();
+        my $logged_in_user = CXGN::People::Person->new($c->dbc->dbh(), $logged_in_person_id);
+        my $initials = substr($logged_in_user->get_first_name(), 0, 1) . substr($logged_in_user->get_last_name(), 0, 1);
+        $c->stash->{prefix} = $initials;
+    }
+
+    $c->stash->{template} = '/help/sample_templates.mas';
 }
 
 1;
