c70e4d0282cc78ac8b5b5972bf1e66886692c021
David Waring <djw64@cornell.edu>
2023-09-10 17:32:21 -0400

    Load Composed Trait Script: add linked composed trait

--- bin/load_composed_trait.pl
+++ bin/load_composed_trait.pl
@@ -6,7 +6,7 @@ load_composed_trait.pl
 
 =head1 SYNOPSIS
 
-load_composed_trait.pl -H [dbhost] -D [dbname] -t [trait name] -d [trait description]
+load_composed_trait.pl -H [dbhost] -D [dbname] -t [trait name] -d [trait description] -c [trait components]
 
 =head1 COMMAND-LINE OPTIONS
 
@@ -14,10 +14,12 @@ load_composed_trait.pl -H [dbhost] -D [dbname] -t [trait name] -d [trait descrip
  -D  database name
  -t  trait name
  -d  (optional) trait description
+ -c  (optional) components as a comma-separated string of cvterm ids
 
 =head2 DESCRIPTION
 
-Add a free-text trait (not connected to any other ontology terms) into the composed trait ontology
+Add a trait into the composed trait ontology.  If component cvterms are provided, the trait will be 
+linked to the cvterms.  Otherwise, it will be not connected to any existing ontology terms.
 
 =head2 AUTHOR
 
@@ -35,17 +37,17 @@ use CXGN::DB::Connection;
 use CXGN::Onto;
 use Try::Tiny;
 
-our ( $opt_H, $opt_D, $opt_t, $opt_d );
-getopts('H:D:t:d:');
+our ( $opt_H, $opt_D, $opt_t, $opt_d, $opt_c );
+getopts('H:D:t:d:c:');
 
 sub print_help {
     print STDERR
-"A script to load a free-text trait into the composed trait ontology\nUsage: load_composed_trait.pl -H [dbhost] -D [dbname] -T [trait name] \n";
+"A script to load a trait into the composed trait ontology\nUsage: load_composed_trait.pl -H [dbhost] -D [dbname] -t [trait name] -d [trait description] -c [trait components] \n";
 }
 
 if ( !$opt_D || !$opt_H || !$opt_t ) {
     print_help();
-    die("Exiting: -H [dbhost] or -D [dbname] or -T [trait name] options missing\n");
+    die("Exiting: -H [dbhost] or -D [dbname] or -t [trait name] options missing\n");
 }
 
 my $dbh = CXGN::DB::InsertDBH->new(
@@ -60,10 +62,18 @@ my $dbh = CXGN::DB::InsertDBH->new(
 );
 
 my $schema = Bio::Chado::Schema->connect( sub { $dbh->get_actual_dbh() } );
+$dbh->do('SET search_path TO public,sgn');
 
 my $coderef = sub {
     my $onto = CXGN::Onto->new({ schema => $schema });
-    $onto->store_breeder_term($opt_t, $opt_d);
+    if ( $opt_c ) {
+        my %t;
+        $t{$opt_t} = $opt_c;
+        $onto->store_composed_term(\%t);
+    }
+    else {
+        $onto->store_breeder_term($opt_t, $opt_d);
+    }
 };
 
 try {
