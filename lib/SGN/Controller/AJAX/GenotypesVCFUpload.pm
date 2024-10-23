1fd95737651377d31571e5c68db8b21185ed1981
Clay Birkett <clb343@cornell.edu>
2024-10-23 13:02:32 -0400

    revert changes, now same as sgn branch

--- lib/SGN/Controller/AJAX/GenotypesVCFUpload.pm
+++ lib/SGN/Controller/AJAX/GenotypesVCFUpload.pm
@@ -568,10 +568,9 @@ sub upload_genotype_verify_POST : Args(0) {
                     foreach my $error ( sort @mismatch_marker_names) {
                         $marker_name_error .= $error."<br>";
                     }
-                    if (!$accept_warnings){
-                        $c->stash->{rest} = { error => "These marker names in your file are not in the selected protocol. $marker_name_error"};
-                        $c->detach();
-		    }
+
+                    $c->stash->{rest} = { error => "These marker names in your file are not in the selected protocol. $marker_name_error"};
+                    $c->detach();
                 }
 
 
